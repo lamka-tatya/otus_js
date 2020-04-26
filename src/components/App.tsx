facf1d01a40ea089d0e4b65dd2b24316cf88237b
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-04-27 02:03:58 +0300

    remove old code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,7 @@
-import React, { FC, Component } from "react";
-import { Input } from "./Input/Input";
-import { HelloButton } from "./HelloButton/HelloButton";
-import { Output } from "./Output/Output";
+import React, { Component } from "react";
 import Field from "./Field/Field";
 
 export class App extends Component {
-  constructor(props: any) {
-    super(props);
-    this.onClickHelloButton = this.onClickHelloButton.bind(this);
-    this.onChangeInput = this.onChangeInput.bind(this);
-  }
-  state = {
-    userName: "",
-  };
-
-  inputValue = "";
-
-  onClickHelloButton() {
-    this.setState({ userName: this.inputValue });
-  }
-
-  onChangeInput(inputValue: string) {
-    this.inputValue = inputValue;
-  }
-
   render() {
     return <Field rowCount={5} columnCount={6} emptyPercent={0} />;
   }
