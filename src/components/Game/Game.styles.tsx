c689387fadf0f9c8f4e2019c81f12071ded36783
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-05-18 01:15:15 +0300

    ya u mamy designer

--- src/components/Game/Game.styles.tsx
+++ src/components/Game/Game.styles.tsx
@@ -9,28 +9,34 @@ export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 95%;
+  width: 100%;
 `;
 
 export const FieldContainer = styled.div`
+  z-index: 1;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
   background-color: #cccccc;
-  border-right: 2px solid grey;
-  border-bottom: 2px solid grey;
+  border-right: 2px solid #c3c3c3;
+  border-bottom: 2px solid #c3c3c3;
   border-radius: 0 0 20px 0;
+  box-shadow: 0px 0px 10px 18px #f1f1f1;
 `;
 
 export const SettingsContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 5%;
+  background-color: #ece8e8;
+  width: 80px;
 `;
 
 export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: flex-start;
+  align-items: center;
+  height: 80px;
+  background-color: #ece8e8;
 `;
