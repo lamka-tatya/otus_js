027809a56290685e9de3eca5e1050e8e1fdcc75a
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-06-13 16:53:05 +0300

    move styleMock to jest directory

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js"
   },
   moduleNameMapper: {
-	"\\.(css|less)$": "<rootDir>/styleMock.js",
+	"\\.(css|less)$": "<rootDir>/jest/styleMock.js",
 	"^@/(.*)$": "<rootDir>/src/components/$1",
   },
 };
