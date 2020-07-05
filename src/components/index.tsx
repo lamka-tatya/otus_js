1349340c7b8556874482abc7b84eb0fa7838ad4c
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-07-06 01:51:16 +0300

    move App to store

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,14 +2,18 @@ import React, { FC } from "react";
 import { StartScreen } from "./screens/StartScreen";
 import { GameScreen } from "./screens/GameScreen";
 import { Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 export const App: FC = () => (
-  <BrowserRouter>
-    <Route path="/" exact>
-      <StartScreen />
-    </Route>
-    <Route path="/game">
-      <GameScreen />
-    </Route>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" exact>
+        <StartScreen />
+      </Route>
+      <Route path="/game">
+        <GameScreen />
+      </Route>
+    </BrowserRouter>
+  </Provider>
 );
